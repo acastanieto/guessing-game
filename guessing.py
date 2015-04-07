d59eaf39eb528cd2daac5745068387996169eae0
Hackbright Student <info@hackbrightacademy.com>
2015-04-07 16:17:47 -0700

    Added while loop and if statements

--- guessing.py
+++ guessing.py
@@ -4,7 +4,6 @@ name = raw_input("Hi!  What's your name? ")
 
 print "Hi, %s!" % name
 print "%s, I'm thinking of a random number between 1 and 100. Try to guess my number." % name
-print "Try to guess my number."
 
 guess = raw_input("Your guess? ") 
 
@@ -12,4 +11,13 @@ import random
 
 answer =  random.randint(1,100)
 
-print answer 
+while guess != answer:
+    if guess > answer:
+        print "Your guess is too high.  Try again."
+        guess = raw_input("Your next guess?")    
+    elif guess < answer:
+        print "Your guess is too low.  Try again."
+    elif guess == answer:
+        print "That's the correct guess!  Yay, you win everything!"
+        break
+
