6495154c39b8b585c981b3e5afc4478499ca43fb
Hackbright Student <info@hackbrightacademy.com>
2015-04-07 16:28:57 -0700

    Moved guess input into high and low conditions, works but no congratulatory message

--- guessing.py
+++ guessing.py
@@ -1,3 +1,5 @@
+import random 
+
 print "This is the Guessing Game!!"
 
 name = raw_input("Hi!  What's your name? ") 
@@ -5,21 +7,29 @@ name = raw_input("Hi!  What's your name? ")
 print "Hi, %s!" % name
 print "%s, I'm thinking of a random number between 1 and 100. Try to guess my number." % name
 
+answer =  random.randint(1,100)
+
+print "The answer is %d." % answer 
+
 guess = int(raw_input("Your guess? ")) 
 
-import random 
-
-answer =  random.randint(1,100)
-print answer 
-
 while guess != answer:
+    
     if guess > answer:
+    
         print "Your guess is too high.  Try again."
-        guess = raw_input("Your next guess?")    
+          
+        guess = int(raw_input("Your next guess?"))  
+
     elif guess < answer:
+        
         print "Your guess is too low.  Try again."
+
+        guess = int(raw_input("Your next guess?"))  
    
     elif guess == answer:
+        
         print "That's the correct guess!  Yay, you win everything!"
+        
         break
 
