cbd786e2caeadc5397b62c439fed5df537b068d1
Hackbright Student <info@hackbrightacademy.com>
2015-04-07 16:06:18 -0700

    Added random number generator

--- guessing.py
+++ guessing.py
@@ -3,3 +3,13 @@ print "This is the Guessing Game!!"
 name = raw_input("Hi!  What's your name? ") 
 
 print "Hi, %s!" % name
+print "%s, I'm thinking of a random number between 1 and 100. Try to guess my number." % name
+print "Try to guess my number."
+
+guess = raw_input("Your guess? ") 
+
+import random 
+
+answer =  random.randint(1,100)
+
+print answer 
