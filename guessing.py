22a612dff14ac9e6381dcc2e7804ccd097c27052
Hackbright Student <info@hackbrightacademy.com>
2015-04-07 16:24:13 -0700

    Changed input into an integer

--- guessing.py
+++ guessing.py
@@ -5,11 +5,12 @@ name = raw_input("Hi!  What's your name? ")
 print "Hi, %s!" % name
 print "%s, I'm thinking of a random number between 1 and 100. Try to guess my number." % name
 
-guess = raw_input("Your guess? ") 
+guess = int(raw_input("Your guess? ")) 
 
 import random 
 
 answer =  random.randint(1,100)
+print answer 
 
 while guess != answer:
     if guess > answer:
@@ -17,6 +18,7 @@ while guess != answer:
         guess = raw_input("Your next guess?")    
     elif guess < answer:
         print "Your guess is too low.  Try again."
+   
     elif guess == answer:
         print "That's the correct guess!  Yay, you win everything!"
         break
